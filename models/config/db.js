5c11f85fcb72ecb2c26fa6b4729b655f509f6687
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-19 02:17:01 -0600

    Update db.js

--- models/config/db.js
+++ models/config/db.js
@@ -1,16 +1,22 @@
 const { Pool } = require('pg');
 
+// Usamos async/await para mejorar el manejo de la conexión
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Esto usa la URL de la base de datos de Render
-  ssl: { rejectUnauthorized: false }  // Esto asegura la conexión segura
+  connectionString: process.env.DATABASE_URL, // Usa la URL de la base de datos de Render desde las variables de entorno
+  ssl: { rejectUnauthorized: false }  // Asegura la conexión segura
 });
 
-pool.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-  } else {
+// Función para verificar la conexión de la base de datos
+const connectToDatabase = async () => {
+  try {
+    await pool.connect();
     console.log('Conectado a la base de datos PostgreSQL.');
+  } catch (err) {
+    console.error('Error al conectar a la base de datos:', err);
   }
-});
+};
+
+connectToDatabase();
 
 module.exports = pool;
+
