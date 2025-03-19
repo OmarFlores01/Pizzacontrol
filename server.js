f27dd58858c65a60a537a6c81a092af962ed74cc
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-19 01:44:53 -0600

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();  // Asegúrate de que esta línea esté al principio del archivo
 
 const express = require('express');
-const cors = require('cors'); //Middleware que permite hacer peticiones   app.use()/Ejecuta código en cada petición    
+const cors = require('cors'); // Middleware que permite hacer peticiones
 const path = require('path'); // Módulo de Node.js para manejar rutas de archivos y directorios.
+const bodyParser = require('body-parser'); // Middleware para procesar cuerpos de solicitudes
 
-//Cada archivo en routes/ contiene las definiciones de las rutas para diferentes partes del sistema
+// Cada archivo en routes/ contiene las definiciones de las rutas para diferentes partes del sistema
 const authRoutes = require('./routes/authRoutes');
 const pedidoRoutes = require('./routes/pedidoRoutes');
 const productoRoutes = require('./routes/productoRoutes');
@@ -12,11 +13,16 @@ const inventarioRoutes = require('./routes/inventarioRoutes');
 const clienteRoutes = require('./routes/clienteRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Usar puerto definido en el archivo .env si está presente
 
-// Sirve archivos estáticos sean accesibles
+// Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Esto permite que las solicitudes JSON sean procesadas correctamente
+
+// Middleware para procesar solicitudes con bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sirve archivos estáticos para que sean accesibles
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/views', express.static(path.join(__dirname, 'views')));
 
@@ -25,14 +31,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
-// permiten a los clientes/empleados interactuar con el servidor.
+// Rutas de la API
 app.use('/api/auth', authRoutes);
 app.use('/api/pedidos', pedidoRoutes);
 app.use('/api/productos', productoRoutes);
 app.use('/api/inventario', inventarioRoutes);
 app.use('/api/cliente', clienteRoutes);
 
-
+// Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
